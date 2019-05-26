00c7b95f476970944633e81f2ef2e3a764304995
Itamir Barroca Filho <itamirbarrocafilho@MacBook-Pro-de-Itamir.local>
2019-05-26 12:51:47 -0300

    bug correction

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
   void _resetCampos() {
     pesoController.clear();
     alturaController.clear();
+    _formKey = GlobalKey<FormState>();
     setState(() {
       _textInfo = "";
     });
